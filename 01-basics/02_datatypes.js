f3371b5546742509a36ddffee8103cade7f9de04
rohit3325-cpu <r2614476@gmail.com>
2024-11-13 19:06:02 +0000

    repo_updated

--- 01-basics/02_datatypes.js
+++ 01-basics/02_datatypes.js
@@ -20,3 +20,16 @@ let state;
 // symbol
 
 console.log(typeof undefined);
+console.log(42 / +0); // Infinity
+console.log(42 / -0); // -Infinity
+
+
+// BigInt
+const x = BigInt(Number.MAX_SAFE_INTEGER); // 9007199254740991n
+x + 1n === x + 2n; // false because 9007199254740992n and 9007199254740993n are unequal
+console.log(x+1n);
+console.log(x+2n);
+
+
+// Number
+Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2; // true because both are 9007199254740992
---
+++ 01-basics/03_conversion.js
@@ -0,0 +1,34 @@
+// let score = "53abc"//->examples
+// let score = null
+// let score = undefined
+//  let score = true
+// let score = "rohit"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInnumber = Number (score)
+
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber)
+
+
+//"33" =>33
+//"33" => NaN
+//true => 1;false=>0;
+
+
+let isLoggedIn = "rohit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false;
+// "" => false
+// "rohit" => true
+
+let someNumber = 33
+
+let stringnumber =  String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
