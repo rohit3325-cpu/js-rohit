3dc6156c18b4b26ae778ce249e3f27c78d503312
rohit3325-cpu <r2614476@gmail.com>
2024-11-13 13:08:03 +0000

    repo updated

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";  //treat all JS code as newer veersion
+
+// alert( 3 + 3) //we are using nodejs ,not browser
+
+console.log( 3+ 
+    3);//code readibility should be high
+console.log("Rohit")
+
+let name = "Rohit"
+let age = 18
+let isloggedin = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined => 
