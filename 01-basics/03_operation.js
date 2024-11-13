e57bc03ff249fae95804fe437d406c97646fd23a
rohit3325-cpu <r2614476@gmail.com>
2024-11-13 19:27:14 +0000

    repo updated

---
+++ 01-basics/03_operation.js
@@ -0,0 +1,45 @@
+// ************** Operations ************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+console.log(22+22);
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hitesh"
+let str2 = " hello"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1" + 2 + 2);
+console.log(1+2+ "2");
+
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
