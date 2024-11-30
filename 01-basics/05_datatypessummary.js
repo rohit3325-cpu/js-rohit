a2d8c028549c742d5b6536c32337fb42d326020e
rohit3325-cpu <r2614476@gmail.com>
2024-11-30 19:41:11 +0000

    repo changed

--- 01-basics/05_datatypessummary.js
+++ 01-basics/05_datatypessummary.js
@@ -1,4 +1,4 @@
- //# Primitive
+ //# Primitive->original value doesnot changed but a copy formed
 
  // 7 types: string, Number, Boolean, null, Undefined, Symbol,BigInt
 
@@ -16,7 +16,7 @@ console.log(id === anotherid);
 
 const bigNumber = 345678912n
 
- //Reference (Non primitive)
+ //Reference (Non primitive->we made changes and it also changes the actual value)
 
  //Array, objects,Function
 
@@ -36,3 +36,27 @@ const bigNumber = 345678912n
  console.log(typeof bigNumber);
  console.log(typeof anotherid);
  console.log(typeof myFunction);
+
+ // +++++++++++++++++++++++++++++++++++++++++++
+ //memory
+ //stack(Primitive),heap(non primitive)
+ 
+ let myYoutubename = "rohit.com"
+
+ let anothername = myYoutubename
+ anothername="chitrakut"
+
+ console.log(myYoutubename);
+ console.log(anothername);
+ 
+let userOne = {
+   email : "rohti@gmail.com",
+   upi : "r213@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "fjwj@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
