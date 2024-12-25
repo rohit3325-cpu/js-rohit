71b33a1f135119f30e517f10972df17650f73cf2
rohit3325-cpu <r2614476@gmail.com>
2024-12-25 11:33:12 +0000

    repo updated

---
+++ 02-basics/01_arrays.js
@@ -0,0 +1,34 @@
+//Array
+
+const myArr = [0,1,2,3,4,5,true,"Rohit"]
+const myHeros = ["thor","krish"]
+const mrArr2 = new Array(1,2,3,4)
+console.log(myArr[7]);
+
+//Array Methods
+
+// myArr.push(7)
+// myArr.push("raj")
+// myArr.pop()
+
+// myArr.unshift(9)//it take to much time
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// console.log(newArr);
+
+// slice ,splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
