f9e6b965271e411b2c80446b35ffc22e1e61a151
rohit3325-cpu <r2614476@gmail.com>
2024-12-25 19:10:20 +0000

    repo update

---
+++ 03-basics/01_function.js
@@ -0,0 +1,24 @@
+
+
+function anyname(){
+    console.log("R");
+console.log("o");
+console.log("H");
+console.log("I");
+console.log("T");
+
+}
+
+// anyname()
+
+function addTwoNumber(num1,num2){
+   console.log(num1 + num2);
+   
+}
+
+addTwoNumber(3,"a")
+addTwoNumber(3,4)
+addTwoNumber(3,null)
+
+// inputs taken at time of function definition is parameter
+//when we call there we take some parametrs i.e arguments
