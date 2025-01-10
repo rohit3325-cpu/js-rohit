e3b201f6105fad94dc4378d1bc4fa2b84a8aaf0b
rohit3325-cpu <r2614476@gmail.com>
2025-01-10 14:44:53 +0000

    repo update

--- 09-advance-one/promise.js
+++ 09-advance-one/promise.js
@@ -36,3 +36,78 @@ promisethree.then(function(user){
     console.log(user);
     
 })
+
+const promiseFour = new Promise(function(resolve,reject){
+     setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username:"ROhit", password: "1234"})
+        }else{
+            reject('ERROR : Something went wrong')
+        }
+     },1000)
+})
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+       console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or reject"))
+
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username:"Javascript", password: "1234"})
+        }else{
+            reject('ERROR : JS went wrong')
+        }
+     },1000)
+});
+
+
+// doing with the help of async and await method
+
+// async function consumePromiseFive(){
+//    const response = await  promiseFive
+//    console.log(response);
+// }
+// here it will not execute if error is false because async ,await take it as it will always true. so here we use trycatch method
+async function consumePromiseFive(){
+  try {
+    const response = await  promiseFive
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+    
+  }
+ }
+
+consumePromiseFive()
+
+// async function getallUser(){
+//    try {
+//     const response  =await fetch('https://api.github.com/users/Rahulm2310')
+//     const data = await response.json()//here we use await because json takes time
+//     console.log(data);
+//    } catch (error) {
+//     console.log("E: ",error);
+//    }
+// }
+// getallUser()
+
+fetch('https://api.github.com/users/Rahulm2310')
+.then( () => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
